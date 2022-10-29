df7666806c9ad6ba5c1b9bc2da13e1542c049b23
Joao <bcastillo-2022474@kinal.edu.gt>
2022-10-28 19:08:09 -0600

    trying connecting DB

--- src/model/DAO/connection.js
+++ src/model/DAO/connection.js
@@ -1,8 +1,9 @@
 import mysql from "mysql2/promise.js";
 
+// mysql://b8bfa4a33cc0da:22fb4b2b@us-cdbr-east-06.cleardb.net/heroku_9a3b3cb0ffdbe76?reconnect=true
 export const pool = await mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "books_directory",
+  host: "us-cdbr-east-06.cleardb.net",
+  user: "b8bfa4a33cc0da",
+  password: "22fb4b2b",
+  database: "heroku_9a3b3cb0ffdbe76",
 });
